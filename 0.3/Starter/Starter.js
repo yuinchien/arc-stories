b33707c585e92275f573529907baad2aa18cd7c6
Yuin Chien <tinyuin.chien@gmail.com>
2018-01-19 09:38:51 -0800

    Created Top Stories recipe using NYTimes API

--- 0.3/Starter/Starter.js
+++ 0.3/Starter/Starter.js
@@ -12,26 +12,112 @@ defineParticle(({DomParticle}) => {
 
   let host = 'starter';
 
+  let storyTemplate = `
+<style>
+[story] {
+  color: rgba(0,0,0,.87);
+  display: flex;
+  flex-direction: row;
+  border-top: 1px solid rgba(0,0,0,.1);
+  border-left: 1px solid rgba(0,0,0,.1);
+  border-right: 1px solid rgba(0,0,0,.1);
+  margin: 0 16px;
+  padding: 16px;
+}
+[thumb] {
+  width: 100px;
+  height: 100px;
+  margin-left: 16px;
+  background-size: cover;
+  background-position: 50% 50%;
+  background-color: #eee;
+  outline: 1px solid rgba(0,0,0,.1);
+  outline-offset: -1px;
+}
+
+</style>
+ <div story>
+   <div style="flex:1;">
+    <div style="font-size: 14px; line-height: 14px; color: rgba(0,0,0,.54); margin-bottom: 8px;">{{section}}</div>
+    <div style="font-size: 18px; line-height: 26px;">{{title}}</div>
+   </div>
+   <div thumb style="{{imgStyle}}"></div>
+ </div>
+   `.trim();
+
   let template = `
 <style>
   [${host}] {
-    padding: 8px;
+    padding: 0px;
+  }
+  [${host}] [caption] {
+    font-size: 32px;
+    margin: 16px;
+    padding-right: 80px;
+    flex: 1;
+  }
+  [${host}] [logo] {
+    width: 24px;
+  }
+  [logoWrapper] {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding: 0 16px;
   }
 </style>
-
 <div ${host}>
-  Hello <span>{{world}}</span>
+  <div logoWrapper>
+    <img logo src="http://1000logos.net/wp-content/uploads/2017/04/Symbol-New-York-Times.png" />
+    <div caption>Top Stories</div>
+  </div>
+  <template storys>${storyTemplate}</template>
+  <div style="border-bottom: 1px solid rgba(0,0,0,.1);">{{storys}}</div>
 </div>
     `.trim();
 
   return class extends DomParticle {
+
+    constructor(props) {
+      super(props);
+      this._setState({data: []});
+      this.httpRequest = null;
+      this.testVarible= "this is a test";  // I declare the variable here
+      // this.makeRequest();
+    }
+
     get template() {
       return template;
     }
+
+    async makeRequest() {
+      console.log('makeRequest');
+      var url = "https://api.nytimes.com/svc/topstories/v2/home.json"+"?api-key=b7c19961dab74132b69551bfbc1cdf9f";
+
+      let response = await fetch(url);
+      let json = await response.json();
+      console.log(json.results);
+      this._setState({data: json.results});
+    }
+
     _render(props, state) {
+      // return {
+      //   data: state.data
+      // };
       return {
-        world: 'World'
+        storys: {
+          $template: 'storys',
+          models: state.data.map(story => {
+            return {
+              // isBriefing: story.section=='Briefing',
+              title: story.title,
+              section: story.section,
+              imgStyle: 'background-image:url('+ story.multimedia[ story.multimedia.length-1 ].url +');',
+            };
+          })
+        }
       };
+
     }
   };
 
